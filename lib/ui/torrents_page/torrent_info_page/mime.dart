6eb86728681293b099a033315cceec84746ad657
YouROK <8yourok8@mail.ru>
2025-11-06 22:39:17 +0300

    rem unused

--- lib/ui/torrents_page/torrent_info_page/mime.dart
+++ lib/ui/torrents_page/torrent_info_page/mime.dart
@@ -14,15 +14,6 @@ class Mime {
     return "*/*";
   }
 
-  static String _getExtension(String filepath) {
-    final fileName = filepath.split(RegExp(r'[\\/]')).last;
-    final dotIndex = fileName.lastIndexOf('.');
-    if (dotIndex == -1 || dotIndex == fileName.length - 1) {
-      return '';
-    }
-    return fileName.substring(dotIndex + 1);
-  }
-
   static const _extVideo = ["3g2", "3gp", "aaf", "asf", "avchd", "avi", "drc", "flv", "iso", "m2v", "m2ts", "m4p", "m4v", "mkv", "mng", "mov", "mp2", "mp4", "mpe", "mpeg", "mpg", "mpv", "mxf", "nsv", "ogg", "ogv", "ts", "qt", "rm", "rmvb", "roq", "svi", "vob", "webm", "wmv", "yuv"];
 
   static const _extAudio = ["aac", "aiff", "ape", "au", "flac", "gsm", "it", "m3u", "m4a", "mid", "mod", "mp3", "mpa", "pls", "ra", "s3m", "sid", "wav", "wma", "xm"];
