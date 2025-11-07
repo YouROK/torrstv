605b404e55be19362a1db2440e726e4dff42c3df
YouROK <8yourok8@mail.ru>
2025-11-08 00:21:25 +0300

    add player settings

--- lib/core/settings/settings_providers.dart
+++ lib/core/settings/settings_providers.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:torrstv/core/settings/settings.dart';
+import 'package:torrstv/core/settings/videoplayer_settings.dart';
 
 final sharedPreferencesProvider = Provider<SharedPreferences>((ref) {
   throw UnimplementedError('SharedPreferences must be initialized in main');
@@ -9,4 +10,9 @@ final sharedPreferencesProvider = Provider<SharedPreferences>((ref) {
 final settingsProvider = Provider<Settings>((ref) {
   final prefs = ref.watch(sharedPreferencesProvider);
   return Settings(prefs);
+});
+
+final videoPlayerSettingsProvider = Provider<VideoPlayerSettings>((ref) {
+  final prefs = ref.watch(sharedPreferencesProvider);
+  return VideoPlayerSettings(prefs);
 });
