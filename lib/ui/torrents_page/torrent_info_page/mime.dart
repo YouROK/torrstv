8c095663397e16149f328fe45b46dd99ec2e89b4
YouROK <yuri.yushkin@aisa.ru>
2025-11-14 15:17:24 +0300

    remove iso from video

--- lib/ui/torrents_page/torrent_info_page/mime.dart
+++ lib/ui/torrents_page/torrent_info_page/mime.dart
@@ -17,7 +17,43 @@ class Mime {
     return "*/*";
   }
 
-  static const _extVideo = ["3g2", "3gp", "aaf", "asf", "avchd", "avi", "drc", "flv", "iso", "m2v", "m2ts", "m4p", "m4v", "mkv", "mng", "mov", "mp2", "mp4", "mpe", "mpeg", "mpg", "mpv", "mxf", "nsv", "ogg", "ogv", "ts", "qt", "rm", "rmvb", "roq", "svi", "vob", "webm", "wmv", "yuv"];
+  static const _extVideo = [
+    "3g2",
+    "3gp",
+    "aaf",
+    "asf",
+    "avchd",
+    "avi",
+    "drc",
+    "flv",
+    "m2v",
+    "m2ts",
+    "m4p",
+    "m4v",
+    "mkv",
+    "mng",
+    "mov",
+    "mp2",
+    "mp4",
+    "mpe",
+    "mpeg",
+    "mpg",
+    "mpv",
+    "mxf",
+    "nsv",
+    "ogg",
+    "ogv",
+    "ts",
+    "qt",
+    "rm",
+    "rmvb",
+    "roq",
+    "svi",
+    "vob",
+    "webm",
+    "wmv",
+    "yuv",
+  ];
 
   static const _extAudio = ["aac", "aiff", "ape", "au", "flac", "gsm", "it", "m3u", "m4a", "mid", "mod", "mp3", "mpa", "pls", "ra", "s3m", "sid", "wav", "wma", "xm", "mka"];
 
